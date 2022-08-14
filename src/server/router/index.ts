f5027f14cd5e72c90d62d85b7abce9a59932d3d4
daxdiv <95906740+daxdiv@users.noreply.github.com>
2022-08-14 17:06:32 +0200

    create new paste

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,12 +1,11 @@
 // src/server/router/index.ts
 import { createRouter } from "./context";
 import superjson from "superjson";
-
-import { exampleRouter } from "./example";
+import { pasteRouter } from "./paste";
 
 export const appRouter = createRouter()
-  .transformer(superjson)
-  .merge("example.", exampleRouter);
+    .transformer(superjson)
+    .merge("paste.", pasteRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
