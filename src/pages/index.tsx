ecd4d895b42f06ef2cc02fab8c6da34cb13b8213
pollettoimpanato <david200288l@gmail.com>
2022-08-17 17:39:36 +0200

    delete paste after a selected amount of time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { trpc } from "../utils/trpc";
+import type { TExpiresIn } from "../utils/types";
 
 const Home: NextPage = () => {
     const [pasteText, setPasteText] = useState<string>("");
     const router = useRouter();
     const createPasteMutation = trpc.useMutation("paste.create");
+    const selectExpireTimeRef = useRef<HTMLSelectElement>(null);
+
+    const customArrowStyles =
+        "after:content-[''] after:w-0 after:h-0 after:border-l-4 after:border-r-4 after:border-t-4 after:border-l-transparent after:border-r-transparent after:border-t-white after:z-10 relative after:absolute after:top-2 after:right-[-75%]";
 
     const handleSubmit = (e: React.MouseEvent) => {
         e.preventDefault();
@@ -14,7 +19,10 @@ const Home: NextPage = () => {
         if (pasteText === "") return;
 
         createPasteMutation.mutate(
-            { text: pasteText },
+            {
+                text: pasteText,
+                expiresIn: selectExpireTimeRef.current?.value as TExpiresIn,
+            },
             {
                 onSuccess: ({ id }) => {
                     router.push(`/pastes/${id}`);
@@ -33,6 +41,25 @@ const Home: NextPage = () => {
                 onChange={e => setPasteText(e.target.value)}
                 className="p-2 bg-gray-700 border-2 border-white rounded-md"
             ></textarea>
+
+            <div className="flex justify-center items-center mt-4 gap-2">
+                <p className={`font-bold text-sm ${customArrowStyles}`}>
+                    Paste expires in:{" "}
+                </p>
+                <select
+                    className="appearance-none bg-gray-700 border-2 pl-2 pr-4 text-sm border-white text-center font-bold rounded-xl relative cursor-pointer"
+                    name="select-expire-time"
+                    defaultValue="1h"
+                    ref={selectExpireTimeRef}
+                >
+                    <option value="1h">1 hour</option>
+                    <option value="1d">1 day</option>
+                    <option value="1w">1 week</option>
+                    <option value="1m">1 month</option>
+                    <option value="1y">1 year</option>
+                </select>
+            </div>
+
             <button
                 className="font-bold px-2 py-1 cursor-pointer mt-4 bg-cyan-700 border-2 border-white rounded-xl w-24 hover:scale-105 hover:bg-cyan-500 d transition-all"
                 disabled={createPasteMutation.isLoading}
